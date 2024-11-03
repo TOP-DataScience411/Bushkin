6e0d23dadb9273a0b4c8f6baf41d68140e9b060d
Gennadiy <renardes@gmail.com>
2024-11-03 12:33:33 +0500

    2024.10.27: правки оформления

--- 2024.10.27/4.py
+++ 2024.10.27/4.py
@@ -2,9 +2,11 @@ num = int(input())
 num1 = num // 100
 num2 = num // 10 % 10
 num3 = num % 10
-print( f'Сумма цифр = { num1 + num2 + num3 }')
-print( f'Произведение цифр = { num1 * num2 * num3 }')
+print(f'Сумма цифр = {num1 + num2 + num3}')
+print(f'Произведение цифр = {num1 * num2 * num3}')
+
 
 # 333
 # Сумма цифр = 9
 # Произведение цифр = 27
+
--- 2024.10.27/5.py
+++ 2024.10.27/5.py
@@ -1,8 +1,10 @@
 int_mile = input()
 fract_mile = input()
-mile = float( int_mile + '.' + fract_mile)
-print( f'{mile} миль = { round(mile * 1.61,1) } км')
+mile = float(int_mile + '.' + fract_mile)
+print(f'{mile} миль = {round(mile * 1.61, 1)} км')
+
 
 # 15
 # 7
 # 15.7 миль = 25.3 км
+
