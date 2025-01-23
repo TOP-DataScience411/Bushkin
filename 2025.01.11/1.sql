08ab87a24d7de2d50d0285e9c8b2760206a7f144
Vladislav732010 <volgograd732010@yandex.ru>
2025-01-23 19:54:53 +0300

    домашнее задание

--- 2025.01.11/1.sql
+++ 2025.01.11/1.sql
@@ -3,6 +3,47 @@
 
 -- 1. Вывести названия всех стран Евразии
 
- SELECT name
- FROM country
- WHERE Continent = 'Asia' OR Continent = 'Europe';
+  SELECT Name
+  FROM country
+  WHERE Continent = 'Asia' OR Continent = 'Europe';
+
+
+-- 2. Вывести названия регионов и стран, в которых ожидаемая продолжительность жизни меньше пятидесяти лет
+
+  SELECT region,Name
+  FROM country
+  WHERE LifeExpectancy < 50;
+
+
+-- 3. Вывести название самой крупной по площади страны Африки
+
+  SELECT Name
+  FROM country
+  WHERE Continent ='Africa'
+  ORDER BY SurfaceArea DESC
+  LIMIT 1;
+
+
+-- 4. Вывести названия пяти азиатских стран с самой низкой плотностью населения
+
+  SELECT Name
+  FROM country
+  WHERE Continent = 'Asia'
+  ORDER BY Population / SurfaceArea
+  LIMIT 5;
+
+-- 5. Вывести в порядке возрастания населения коды стран и названия городов, численность населения которых превышает пять миллионов человек
+
+
+  SELECT CountryCode, Name
+  FROM city
+  WHERE Population > 5000000
+  ORDER BY Population;
+
+-- 6. Вывести название города в Индии с самым длинным названием
+      для подсчёта количества символов используйте встроенную функцию char_length()
+
+  SELECT Name
+  FROM city 
+  ORDER BY char_length(Name) DESC
+  LIMIT 1;
