2fae7928125ef7f29881821c8aa53efe39e73345
Vladislav732010 <volgograd732010@yandex.ru>
2025-01-26 00:51:12 +0300

    20 %

---
+++ 2025.01.18/1.sql
@@ -0,0 +1,12 @@
+
+-- 1. Вывести названия стран и названия сопоставленных им столиц
+
+
+  SELECT country.name, 
+         city.name
+  FROM        
+             country 
+  INNER JOIN 
+             city 
+  ON 
+             country.Capital = city.id;
