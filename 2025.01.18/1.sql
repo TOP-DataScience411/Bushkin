0fb9b9ea5ecd23e69015dcb6990cd619c275f222
Vladislav732010 <volgograd732010@yandex.ru>
2025-01-28 21:43:22 +0300

    60 %

--- 2025.01.18/1.sql
+++ 2025.01.18/1.sql
@@ -2,7 +2,30 @@
 -- 1. Вывести названия стран и названия сопоставленных им столиц
 
 
-  SELECT country.name, city.name
+  SELECT    country.name, 
+            city.name
   FROM        country 
   INNER JOIN     city 
   ON           country.Capital = city.id;
+
+-- 2. Сравнить по регионам сумму населения стран и сумму населения городов
+
+
+  SELECT sum(DISTINCT country.Population),
+         sum(city.Population),
+         Region
+  FROM          country 
+  INNER JOIN    city
+  ON            Code = CountryCode
+  GROUP BY      Region;
+
+-- 3. Вывести десять языков, на которых разговаривает больше всего людей
+
+  SELECT  language,
+          sum( Percentage / 100 * c.Population) AS total
+  FROM        countrylanguage AS cl
+  INNER JOIN  country AS c
+  ON          CountryCode = Code
+  GROUP BY    language
+  ORDER BY    total   DESC
+  LIMIT 10;
