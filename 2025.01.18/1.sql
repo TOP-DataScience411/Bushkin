a091e1bc6d9ac4e508136c86edfb1a26b76cb0d9
Vladislav732010 <volgograd732010@yandex.ru>
2025-01-29 22:28:10 +0300

    домашнее задание

--- 2025.01.18/1.sql
+++ 2025.01.18/1.sql
@@ -34,15 +34,25 @@
 --4. Вывести названия специальностей и суммарное количество дней отпусков, в которых были врачи каждой специальности; отсортировать по возрастанию суммарного количества дней отпуска
 
       SELECT   s.name, 
-               round(avg(end_date - start_date)) AS aver
+               sum(end_date - start_date) AS sum_al
         FROM   vacations as v
         JOIN   doctors_specs AS d_s
           ON   v.doctor_id = d_s.doctor_id
         JOIN   specializations AS s
           ON   spec_id = s.id
     GROUP BY   s.name
-    ORDER BY   aver; 
+    ORDER BY   sum_al; 
 
 
 
 --5. Вывести округлённую до целого сумму средств, которую можно выделить на одну палату этого отделения (в зависимости от количества палат в отделении), от всех пожертвований каждому отделению; отсортировать по убыванию найденной суммы
+
+
+      SELECT  d.name, round(sum(DISTINCT amount)/count(DISTINCT w.name)) AS per_ward
+        FROM  departments as d
+        JOIN  donations as don
+          ON  d.id=don.dep_id
+        JOIN  wards as w
+          ON  d.id = w.dep_id
+    GROUP BY  d.name
+    ORDER BY  per_ward DESC;
